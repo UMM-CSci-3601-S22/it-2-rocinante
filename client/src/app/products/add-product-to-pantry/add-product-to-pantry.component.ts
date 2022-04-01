c8de70147f72593e52101f37dc973b5896f959ef
Cole Maxwell <cole.n.maxwell99@gmail.com>
2022-04-01 14:44:32 -0500

    Generate AddProductToPantryComponent

---
+++ client/src/app/products/add-product-to-pantry/add-product-to-pantry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product-to-pantry',
+  templateUrl: './add-product-to-pantry.component.html',
+  styleUrls: ['./add-product-to-pantry.component.scss']
+})
+export class AddProductToPantryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

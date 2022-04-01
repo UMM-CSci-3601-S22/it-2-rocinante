08644ff279635e0ee07594dddb55275270be4bc2
Cole Maxwell <cole.n.maxwell99@gmail.com>
2022-04-01 15:23:07 -0500

    Add addPantryItem() function

--- client/src/app/pantry/pantry.service.ts
+++ client/src/app/pantry/pantry.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Product } from '../products/product';
+import { PantryItem } from './pantryItem';
 
 @Injectable()
 export class PantryService {
   readonly pantryUrl: string = environment.apiUrl + 'pantry';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   getPantryItems(): Observable<Product[]> {
     return this.httpClient.get<Product[]>(this.pantryUrl, {
     });
   }
 
-
+  addPantryItem(newPantryItem: PantryItem): Observable<string> {
+    // Send post request to add a new PantryItem to the user's pantry
+    return this.httpClient.post<{ id: string }>(this.pantryUrl, newPantryItem).pipe(map(res => res.id));
+  }
 }
